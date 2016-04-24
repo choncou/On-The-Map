d70a4e331dec3084af3c67d3650d85cfcdbe2be2
Unathi Chonco <choncou@gmail.com>
2016-04-23 19:53:12 -0700

    redo posting viewcontroller

--- On The Map/PostingViewController.swift
+++ On The Map/PostingViewController.swift
@@ -8,36 +8,112 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class PostingViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var studyQuestionlabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var findOnMapButton: UIRoundedButton!
+    @IBOutlet weak var submitButton: UIRoundedButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var bottomSection: UIView!
+    var location: CLLocation?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        searchTextField.delegate = self
+        urlTextField.delegate = self
+        locationFindUI()
     }
     
+//MARK: UI Setup
+    /**
+     Set up the UI when asking user for location
+     */
+    func locationFindUI() {
+        studyQuestionlabel.hidden = false
+        findOnMapButton.hidden = false
+        searchTextField.hidden = false
+        bottomSection.backgroundColor = bottomSection.backgroundColor?.colorWithAlphaComponent(1)
+        submitButton.hidden = true
+        mapView.hidden = true
+    }
     
-
-    // MARK: - Navigation
+    /**
+     Set up the UI when asking user for link
+     */
+    func linkAskUI() {
+        guard location != nil else { return }
+        cancelButton.tintColor = UIColor.whiteColor()
+        studyQuestionlabel.hidden = true
+        findOnMapButton.hidden = true
+        searchTextField.hidden = true
+        bottomSection.backgroundColor = bottomSection.backgroundColor?.colorWithAlphaComponent(0.5)
+        submitButton.hidden = false
+        mapView.hidden = false
+        urlTextField.hidden = false
+        setUpMap()
+    }
+    
+// MARK: - Navigation
     @IBAction func cancelButtonTap(sender: UIButton) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
-    /*
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func findOnMapTap(sender: UIRoundedButton) {
+        guard let text = searchTextField.text else { return }
+        CLGeocoder().geocodeAddressString(text){placemarks, error in
+            guard error == nil else {
+                print(error)
+                return
+            }
+            guard let place = placemarks?.first else { return }
+            self.location = place.location
+            self.linkAskUI()
+        }
     }
-    */
+    
+    @IBAction func submitButtonTap(sender: UIRoundedButton) {
+        guard let url = urlTextField.text where urlTextField.text != "" else {
+            showAlert("No Link", message: "Please enter a link")
+            self.urlTextField.text = "http://udacity.com"
+            return
+        }
+        print(url)
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "Okay", style: .Default, handler: nil)
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+    }
 
 }
 
 extension PostingViewController: MKMapViewDelegate {
+    /**
+     Show annotation on map
+     */
+    func setUpMap() {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = (location?.coordinate)!
+        mapView.addAnnotation(annotation)
+        let region = MKCoordinateRegionMake((location?.coordinate)!, MKCoordinateSpanMake(0.5, 0.5))
+        mapView.setRegion(region, animated: true)
+    }
+}
+
+extension PostingViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
     
+    func textFieldDidEndEditing(textField: UITextField) {
+        textField.resignFirstResponder()
+    }
 }
---
+++ iOSInjectionProject/build/InjectionBundle.build/Debug-iphonesimulator/InjectionBundle.build/Objects-normal/x86_64/BundleContents.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/choncou/Documents/Development/Xcode\ Projects/On\ The\ Map/iOSInjectionProject/BundleContents.m \
+  /tmp/injectionforxcode/BundleInjection.h \
+  /tmp/injectionforxcode/BundleInterface.h \
+  /Users/choncou/Documents/Development/Xcode\ Projects/On\ The\ Map/iOSInjectionProject/BundleContents.h
